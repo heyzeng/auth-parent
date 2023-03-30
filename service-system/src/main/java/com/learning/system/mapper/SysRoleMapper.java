93bca1cf7fd7853e19d25b306fd5b0e4a667eb22
heyzeng <270806204@qq.com>
2023-03-30 15:50:59 +0800

    change model

---
+++ service-system/src/main/java/com/learning/system/mapper/SysRoleMapper.java
@@ -0,0 +1,14 @@
+package com.learning.system.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.learning.model.system.SysRole;
+import org.springframework.stereotype.Repository;
+
+
+/**
+ * Author:Jude
+ * Date:2023-03-30 下午3:43
+ */
+@Repository
+public interface SysRoleMapper extends BaseMapper<SysRole>{
+}
