5e88d86aa9027964e127ed69f0c9a4be5306038c
heyzeng <270806204@qq.com>
2023-09-12 23:17:35 +0800

    后台信息和登出接口

--- service-system/src/main/java/com/learning/system/controller/IndexController.java
+++ service-system/src/main/java/com/learning/system/controller/IndexController.java
@@ -1,7 +1,9 @@
 package com.learning.system.controller;
 
+import com.baomidou.mybatisplus.extension.api.R;
 import com.learning.common.reslut.Result;
 import io.swagger.annotations.Api;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -27,9 +29,45 @@ public class IndexController {
     public Result login(){
 
 //        {"code":20000,"data":{"token":"admin-token"}}
-        Map<String, String> loginMap = new HashMap<>();
+        Map<String, Object> loginMap = new HashMap<>();
         loginMap.put("token"," admin-token");
         return Result.ok(loginMap);
+    }
+
+    // 前端数据
+    //        {
+//            "code": 20000,
+//                "data": {
+//            "roles": [
+//            "admin"
+//        ],
+//            "introduction": "I am a super administrator",
+//                    "avatar": "https://wpimg.wallstcn.com/f778738c-e4f8-4870-b634-56703b4acafe.gif",
+//                    "name": "Super Admin"
+//        }
+//        }
+
+    /**
+     * 登录信息
+     * @return
+     */
+    @GetMapping("/info")
+    public Result info(){
+
+        HashMap<String, Object> infoMap = new HashMap<>();
+        infoMap.put("roles","admin");
+        infoMap.put("introduction","I am a super administrator");
+        infoMap.put("avatar","https://wpimg.wallstcn.com/f778738c-e4f8-4870-b634-56703b4acafe.gif");
+        infoMap.put("name","Admin");
+        return Result.ok(infoMap);
+    }
+
+//    {"code":20000,"data":"success"}
+
+    @PostMapping("/logout")
+    public Result logout() {
+
+        return Result.ok();
     }
 
 }
